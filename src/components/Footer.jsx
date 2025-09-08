9c0b64a8a76a34d526f5eac851a67f3df93c3bc8
kingpat015 <kingpatrickleimuel15@gmail.com>
2025-09-08 09:15:35 +0800

    updated portfolio

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,15 @@
 // src/components/Footer.jsx
 import { Layout } from "antd";
+import "../styles/Footer.css";
 
 const { Footer: AntFooter } = Layout;
 
 const Footer = () => {
   return (
-    <AntFooter style={{ textAlign: "center" }}>
-      © {new Date().getFullYear()} Your Name. Built with React + Ant Design.
+    <AntFooter className="custom-footer">
+      © {new Date().getFullYear()} <span>PAT-PORT</span>. 
+      Built with <a href="https://react.dev" target="_blank" rel="noreferrer">React</a> + 
+      <a href="https://ant.design" target="_blank" rel="noreferrer"> Ant Design</a>.
     </AntFooter>
   );
 };
